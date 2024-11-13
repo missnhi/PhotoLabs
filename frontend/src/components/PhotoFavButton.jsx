b56a77540c3e066f34a9622140492a89cdda0683
missnhi <nhi.phan.ley@gmail.com>
2024-11-12 19:53:49 -0700

    Add the Favourite Icon

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,12 +4,27 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [isActive, setIsActive] = useState(false);
+  
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+  
+  // The isActive state is toggled when the button is clicked.
+  // The className of the div is conditionally set to
+  // either photo-list__fav-icon-svg or photo-list__fav-icon based
+  // on the isActive state.
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={isActive}/>
       </div>
     </div>
+    
+    //somehow that this below doesn't work
+  // <div className={isActive ? 'photo-list__fav-icon-svg' : 'photo-list__fav-icon'} onClick={handleClick}>
+  //   <FavIcon selected={isActive}/>
+  // </div>
   );
 }
 
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton.jsx";
 
 
 const PhotoListItem = ({ photo }) => {
   const { id, location, imageSource, username, profile } = photo;
   return (
     <div className="photo-list__item">
+      {/*Adding a Favourite Button*/}
+      <PhotoFavButton />
       <img className="photo-list__image" src={imageSource} alt={`${username}'s photo`}/>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={profile} alt={`${username}'s profile`}/>
